3731530e70fd3e395f5d9007b121e5249fb40b38
SamanthaVelasquez4 <samvel200355@gmail.com>
2025-07-15 15:49:01 -0600

    Tipos de datos y variables

--- Semana1/Clase 2/src/Main.java
+++ Semana1/Clase 2/src/Main.java
@@ -3,5 +3,29 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo!");
+
+        //comentario de una linea
+        /*Comentario
+        * de
+        * varias
+        * lineas
+        * */
+
+
+        //Tipos de datos primitivos
+        // TipoDato NombreVarible
+        int numeroEntero; //declaración de una variable
+        boolean bandera; //verdadero o falso
+        float numeroFlotante;
+        double numeroDecimal;
+
+        //inicializar variables
+        numeroEntero = 10;
+        bandera = false;
+        numeroFlotante = 10.25f;
+        numeroDecimal = 15.2;
+
+        //declarar e inicializar
+        char caracter = 'a';
     }
 }
